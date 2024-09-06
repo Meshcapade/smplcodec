5bca00eff932413376fdfbcbd633681189d1b521
Nikolas Hesse <nikolas@meshcapade.com>
2024-09-06 10:26:50 +0200

    feat: add tests for reading skel .smpl file and smplx amass file

--- test/test_codec.py
+++ test/test_codec.py
@@ -46,6 +46,33 @@ def test_read_sample_smplpp():
     assert a.body_translation.shape == (601, 3)
 
 
+def test_read_sample_skel():
+    a = SMPLCodec.from_file(TESTDIR / "avatar_skel.smpl")
+    assert a.smpl_version == SMPLVersion.SKEL
+    assert a.gender == SMPLGender.MALE
+
+    assert a.shape_parameters.shape == (10,)
+    assert a.frame_count == 601
+    assert a.frame_rate == 120.0
+
+    assert a.body_pose.shape == (601, 46)
+    assert a.body_translation.shape == (601, 3)
+
+
+def test_read_amass_sample_smplx():
+    a = SMPLCodec.from_amass_npz(TESTDIR / "apose_to_handsFront_smplx.npz")
+    
+    assert a.smpl_version == SMPLVersion.SMPLX
+    assert a.gender == SMPLGender.NEUTRAL
+
+    assert a.shape_parameters.shape == (10,)
+    assert a.frame_count == 34
+    assert a.frame_rate == 60.0
+
+    assert a.body_pose.shape == (34, 22, 3)
+    assert a.body_translation.shape == (34, 3)
+
+
 def test_full_pose():
     s = SMPLCodec()
     assert s.full_pose.shape == (1, 55, 3)
@@ -82,7 +109,7 @@ def test_validate_succeeds(params):
         {"something_unexpected": None},
         {"smpl_version": None},
         {"smpl_version": -1},
-        {"smpl_version": 5},
+        {"smpl_version": 6},
         {"smpl_gender": -1},
         {"shape_parameters": "something"},
         {"shape_parameters": np.zeros((5, 5))},
