3f3c300d120e81c038f25cd2d54e0d1ee778f2c9
Nikolas Hesse <nikolas@meshcapade.com>
2025-11-27 16:59:17 +0100

    add SMPLXS

--- smplcodec/codec.py
+++ smplcodec/codec.py
@@ -28,6 +28,7 @@ class SMPLVersion(IntEnum):
     SUPR = 3
     SMPLPP = 4
     SKEL = 5
+    SMPLXS = 6
 
     @classmethod
     def from_string(cls, value: str):
@@ -72,6 +73,9 @@ SMPLParamStructure = {
     ),
     SMPLVersion.SMPLPP: PoseParameterSizes(body_pose=(46,)),
     SMPLVersion.SKEL: PoseParameterSizes(body_pose=(46,)),
+    SMPLVersion.SMPLXS: PoseParameterSizes(
+        body_pose=(22, 3), head_pose=(3, 3), left_hand_pose=(15, 3), right_hand_pose=(15, 3)
+    ),
 }
 
 
@@ -82,6 +86,7 @@ SMPLVertexCount = {
     SMPLVersion.SUPR: 10475,
     SMPLVersion.SMPLPP: 35410,
     SMPLVersion.SKEL: 6890,
+    SMPLVersion.SMPLXS: 10475,
 }
 
 
